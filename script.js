47c4ab3dda91e118fe5b06b7e05f03a19fb0419f
Jonathan122802 <jaede2812@gmail.com>
2025-01-05 16:06:35 +0100

    It works

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-var btns;
+var btns,color_cache;
 var index = 0;
 
 function updtBtn(e) {
+    console.log("Hi")
     var key = e.key;
 
     switch(key){
         case "w":
-            index -=4;
+            index -=2;
             break;
         case "a":
             index -= 1;
             break;
         case "s":
-            index +=4;
+            index +=2;
             break;
         case "d":
             index += 1;
+            break;
+        case "Enter":
+            btns[index].click();
+            break;
     }
 
     if (index < 0){
@@ -26,18 +31,35 @@ function updtBtn(e) {
         index = btns.length-1;
     }
 
-    btns[index].style.backgroundColor = "#FB6542";
+    btns[index].style.backgroundColor = update_alpha(color_cache[index]);
+    btns[index].style.transform = "scale(1.03)";
 
     for (let i=0; i<btns.length; i++){
         if (i == index){
             continue;
         }
-        btns[i].style.backgroundColor = "#FFBB00";
+        btns[i].style.backgroundColor = color_cache[i];
+        btns[i].style.transform = "scale(1)";
     }
 }
 
 function loadBtn(){
     index = 0;
     btns = document.getElementsByName("submit");
+    console.log(btns);
+    color_cache = [];
+    for (let i=0; i<btns.length; i++){
+        color_cache.push(getComputedStyle(btns[i]).getPropertyValue("background-color"));
+    }
+    console.log(color_cache);
     document.body.addEventListener("keyup", function (e) {updtBtn(e)});
+}
+
+function update_alpha(rgba){
+    let values = rgba.match(/[\d.]+/g);
+    if (values.length == 3){
+        values.push(0.8);
+    }
+    var color = "rgba("+values.join(",")+")";
+    return color;
 }
